20a81e6e05743b643c6ce03848a5f379a8e7b03a
Gab0410 <gabrielmcorlando@gmail.com>
2026-03-12 21:33:47 -0300

    fix: setup version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def dev_requirements() -> list[str]:
 
 configuration = {
     "name": "core-sg",
-    "version": "0.1.0rc4",
+    "version": "0.1.0rc5",
     "description": "Core-SG graph construction and MST extraction utilities for HDBSCAN-style clustering.",
     "long_description": readme(),
     "long_description_content_type": "text/markdown",
